04db2c0fd3a3ecbfa1ef8f054c27ccf5d6da3f3d
Mika I Huttunen <mikahutt@melkki.cs.helsinki.fi>
2012-05-10 17:17:16 +0300

    mikapallura

---
+++ HienoHarkka/src/hienoharkka/HienoHarkka.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hienoharkka;
+
+/**
+ *
+ * @author mikahutt
+ */
+public class HienoHarkka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Vitonen tuli!11!!!!11");
+    }
+}
